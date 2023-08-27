5e59c817fea4848e24307b4376e922c6e6092895
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2023-08-27 03:36:22 -0300

    Primeira Versão

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>IMC</title>
 </head>
 <body>  
 <h1>IMC JSON</h1>
 <!--- Criei um Formulário para pegar as informções de Altura e Peso e nas propriedade do formulário
 coloquei action que envia para imc.php, onde será tratado todas as informações necessárias!--->
-    <form action="imc.php" method="get">
+<div class="caixa-maior">
+      <form action="imc.php" method="get">
         <label for="altura">Altura:</label>
         <input type="text" id="altura" name="altura" required placeholder="Metros">
         <br>
@@ -18,5 +20,7 @@ coloquei action que envia para imc.php, onde será tratado todas as informaçõe
         <br>
         <button type="submit">Calcular</button>
     </form>
+</div>
+  
 </body>
 </html>
